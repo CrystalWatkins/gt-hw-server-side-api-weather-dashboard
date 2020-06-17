bd7adaf4a36f7f5c466db0147e69daaf0401dd1e
Crystal <crystalwatkins428@gmail.com>
2020-06-16 22:57:00 -0400

    updates- SO many...

--- script.js
+++ script.js
@@ -26,12 +26,13 @@ $("#add-city").on("click", function(event) {
         a.addClass("city");
         a.attr("data-name", city);
         a.text(city)
-        $("#cityList").append(a);
+        $("#cityList").prepend(a);
     
 });
 
 
 function displayWeatherInfo(event, element) {
+
     // console.log(event, element)
     var APIKey = "889cd95742cb4d318b134906ce82bcb0";
     var city = $(element).text();
@@ -42,7 +43,7 @@ function displayWeatherInfo(event, element) {
         method: "GET"
       }).then(function(response) {
 
-        // console.log(response);
+        console.log(response);
 
         $(".currentCityName").append(response.name);
         $("#temp").append("Temperature: " + response.main.temp);
@@ -75,10 +76,60 @@ function displayWeatherInfo(event, element) {
         } else {
           $(uvIndex).addClass("moderate")
         }
-
-
     });
 
+
+   var queryURL3= "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + APIKey;
+   $.ajax({
+    url: queryURL3,
+    method: "GET"
+  }).then(function(response3) {
+    console.log(response3);
+    $("#date1").append("Date: " + response3.list[0].dt_txt);
+    $("#temp1").append("Temp: " + response3.list[0].main.temp);
+    $("#humidity1").append("Humidity: " + response3.list[0].main.humidity);
+
+    var icon1El = response3.list[0].weather[0].icon;
+    var icon1Url = "http://openweathermap.org/img/w/" + icon1El + ".png";
+    $("#icon1").attr("src", icon1Url);
+
+
+    $("#date2").append("Date: " + response3.list[1].dt_txt);
+    $("#temp2").append("Temp: " + response3.list[1].main.temp);
+    $("#humidity2").append("Humidity: " + response3.list[1].main.humidity);
+
+    var icon2El = response3.list[1].weather[0].icon;
+    var icon2Url = "http://openweathermap.org/img/w/" + icon2El + ".png";
+    $("#icon2").attr("src", icon2Url);
+
+    $("#date3").append("Date: " + response3.list[2].dt_txt);
+    $("#temp3").append("Temp: " + response3.list[2].main.temp);
+    $("#humidity3").append("Humidity: " + response3.list[2].main.humidity);
+
+    var icon3El = response3.list[2].weather[0].icon;
+    var icon3Url = "http://openweathermap.org/img/w/" + icon3El + ".png";
+    $("#icon3").attr("src", icon3Url);
+
+    $("#date4").append("Date: " + response3.list[3].dt_txt);
+    $("#temp4").append("Temp: " + response3.list[3].main.temp);
+    $("#humidity4").append("Humidity: " + response3.list[3].main.humidity);
+
+    var icon4El = response3.list[3].weather[0].icon;
+    var icon4Url = "http://openweathermap.org/img/w/" + icon4El + ".png";
+    $("#icon4").attr("src", icon4Url);
+
+    $("#date5").append("Date: " + response3.list[4].dt_txt);
+    $("#temp5").append("Temp: " + response3.list[4].main.temp);
+    $("#humidity5").append("Humidity: " + response3.list[4].main.humidity);
+
+    var icon5El = response3.list[4].weather[0].icon;
+    var icon5Url = "http://openweathermap.org/img/w/" + icon5El + ".png";
+    $("#icon5").attr("src", icon5Url);
   })
+
+  });
+
+
+
   }
 })
